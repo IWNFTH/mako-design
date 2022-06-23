cf275a8a03702fcdf29a677939949489ebedb02a
IWNFTH <taka2superhard@gmail.com>
2022-06-23 22:36:26 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update]
+
   def index
     @posts = Post.all
   end
@@ -12,9 +14,26 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:name, :text)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
